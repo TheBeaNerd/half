85818d77d3704848232ce4e6585517e15c02a4b7
Dave <TheBeaNerd@gmail.com>
2018-09-02 00:48:03 -0500

    new

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-all:
+all: joe.dimacs
 	@echo "Done."
 
+joe.dimacs:
+	./randomSAT.py -o joe.dimacs
+
 clean:
+	$(RM) -rf joe.dimacs
 	$(RM) -rf *.pyc
--- dimacs.py
+++ dimacs.py
@@ -6,9 +6,16 @@ def stringToClause(ln):
     return Set([int(word) for word in ln.split() if int(word) != 0])
 
 def readCNF(file):
-  with open(file) as f:
-    cnf = [stringToClause(line) for line in f if (line[0] != "%" and line[0] != "c" and line[0] != "p")]
-  return cnf
+    with open(file) as f:
+        cnf = [stringToClause(line) for line in f if (line[0] != "%" and line[0] != "c" and line[0] != "p")]
+    sln = []
+    with open(file) as f:
+        for line in f:
+            if line[0:5] == "c SAT":
+                sln += [stringToClause(line[5:])]
+            if (line[0] != "%" and line[0] != "c" and line[0] != "p"):
+                break
+    return (cnf,sln)
 
 @contextlib.contextmanager
 def smart_open(filename=None):
@@ -24,7 +31,8 @@ def smart_open(filename=None):
 
 def printDIMACS(k,n,cnf,sln=[],filename=None):
     with smart_open(filename) as fh:
-        print >>fh, "c " + " ".join('{0}'.format(n) for n in sln)
+        if sln:
+            print >>fh, "c SAT " + " ".join('{0}'.format(n) for n in sln)
         print >>fh, 'p cnf {k} {n}'.format(k=k,n=n)
         for clause in cnf:
             print >>fh, " ".join('{0}'.format(n) for n in clause) + " 0"
